e2897bd4ef8b9f02f87f1bd3f0db712ff3d9db9a
Anup Bagali <anup.bagali1@gmail.com>
2020-05-28 19:27:05 -0400

    change package name

---
+++ reverb_i2c/smbus.py
@@ -0,0 +1,10 @@
+import os
+I2C_SMBUS_BLOCK_MAX = 32
+
+
+class SMBus:
+
+    def __init__(self, bus):
+        print(os.getcwd())
+
+
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="reverb-i2c",
+    name="reverb_i2c",
     version="0.1",
     license="GPL",
     packages=find_packages(),
