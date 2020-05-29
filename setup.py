f3b4d3c92aa7ec05eac8496e789c6c2c031a305e
Anup Bagali <anup.bagali1@gmail.com>
2020-05-28 21:10:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@ setup(
     version="1.0",
     license="GPL",
     packages=find_packages(),
+    author="tjreverb",
+    author_email="tjreverb@gmail.com",
+    description="A fake i2c library for testing purposes",
+    url="https://github.com/TJREVERB/reverb_i2c",
+    python_requires='>=3.7',
 )
